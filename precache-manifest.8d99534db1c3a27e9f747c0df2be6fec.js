604f236e615989138c348b77eb536397140d9f4a
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-02 10:10:18 +0200

    Updates

---
+++ precache-manifest.8d99534db1c3a27e9f747c0df2be6fec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b88a45be6de76dfced4981ee0c8942b",
+    "url": "/%7Bmy-portfolio%7D/index.html"
+  },
+  {
+    "revision": "71f79023b9b8d9b33b28",
+    "url": "/%7Bmy-portfolio%7D/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "d26425caa534989a4870",
+    "url": "/%7Bmy-portfolio%7D/static/css/main.49cb44b2.chunk.css"
+  },
+  {
+    "revision": "71f79023b9b8d9b33b28",
+    "url": "/%7Bmy-portfolio%7D/static/js/2.c64f67cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/%7Bmy-portfolio%7D/static/js/2.c64f67cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26425caa534989a4870",
+    "url": "/%7Bmy-portfolio%7D/static/js/main.dd591b12.chunk.js"
+  },
+  {
+    "revision": "0470f2d9bf63f411ae10",
+    "url": "/%7Bmy-portfolio%7D/static/js/runtime-main.3cc06b42.js"
+  },
+  {
+    "revision": "d6f69ec05fd00c286b54edb34c7adbeb",
+    "url": "/%7Bmy-portfolio%7D/static/media/bckgrnd.d6f69ec0.jpg"
+  },
+  {
+    "revision": "3ac3803d0102e84d6d88a21f00a0df50",
+    "url": "/%7Bmy-portfolio%7D/static/media/bckgrndmenu.3ac3803d.jpg"
+  }
+]);
