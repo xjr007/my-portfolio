cc7f27d0c7398aa74156458abb8bd62aed1b9735
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-03 08:32:50 +0200

    Updates

---
+++ precache-manifest.c2adac95eed33b6c8c1d4d2aa9461963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab76326235da0345d08ba54cd42e2e2",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "58cd0790fc780408bb69",
+    "url": "/my-portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "fec9bb78a8ba1ab82f23",
+    "url": "/my-portfolio/static/css/main.079620d0.chunk.css"
+  },
+  {
+    "revision": "58cd0790fc780408bb69",
+    "url": "/my-portfolio/static/js/2.8812eb2d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-portfolio/static/js/2.8812eb2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec9bb78a8ba1ab82f23",
+    "url": "/my-portfolio/static/js/main.0ba981b9.chunk.js"
+  },
+  {
+    "revision": "29164293d54b9c5dd738",
+    "url": "/my-portfolio/static/js/runtime-main.d891d3af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.956566e6895475659070a9be8fa2008b.js"
+  "/my-portfolio/precache-manifest.c2adac95eed33b6c8c1d4d2aa9461963.js"
 );
 
 self.addEventListener('message', (event) => {
